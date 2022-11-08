3f6b216065c8a5d1d545f033c390f166ef1acae7
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2022-11-08 22:09:32 +0000

    Completed Asset Management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.gif';
+import Data from './data.xml';
+import Notes from './data.csv'
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); 
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(json.title);
+console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
@@ -13,6 +27,9 @@ function component() {
     myIcon.src = Icon
 
     element.appendChild(myIcon)
+
+    console.log(Data)
+    console.log(Notes)
 
     return element
 }
