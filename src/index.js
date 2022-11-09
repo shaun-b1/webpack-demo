39648727ab0a6b184e68c3bb627fff4389d8b113
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2022-11-09 22:17:14 +0000

    Prepared for Output Management

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.gif';
-import Data from './data.xml';
-import Notes from './data.csv'
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); 
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
 
 function component() {
     const element = document.createElement('div');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const myIcon = new Image();
-    myIcon.src = Icon
-
-    element.appendChild(myIcon)
-
-    console.log(Data)
-    console.log(Notes)
 
     return element
 }
